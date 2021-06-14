69846e33ddda018dee51e70e70107070a64b9dd4
Bilal Malik <45467181+bilalmalik4321@users.noreply.github.com>
2021-06-13 22:13:15 -0400

    Update medianOfTwoSortedArrays.java

--- medianOfTwoSortedArrays.java
+++ medianOfTwoSortedArrays.java
@@ -7,34 +7,43 @@ class Solution {
         int nums1Counter=0;
         int nums2Counter=0;
         
+        // get the length of both arrays placed together        
         int combinedLengths=nums1.length + nums2.length;
         
-        ArrayList<Integer> newArray= new ArrayList<Integer>(); 
+        // initialize arrayList to place values from both arrays in order        
+        ArrayList<Integer> combinedOrderedList= new ArrayList<Integer>(); 
         
-        while(newArray.size() != combinedLengths){
+        // while the arrayList is not the size of the combined arrays
+        while(combinedOrderedList.size() != combinedLengths){
+            // if both counters have not reached the end of the array
             if(nums1Counter < nums1.length && nums2Counter < nums2.length){
-                
+                // if the current list1 value is less then current list 2 value
                 if(nums1[nums1Counter] < nums2[nums2Counter]){
-                newArray.add(nums1[nums1Counter]);
+                // add to combined orderedlist
+                combinedOrderedList.add(nums1[nums1Counter]);
                 nums1Counter++;
                 }
                 else if(nums2[nums2Counter] < nums1[nums1Counter]){
-                    newArray.add(nums2[nums2Counter]);
+                    combinedOrderedList.add(nums2[nums2Counter]);
                     nums2Counter++;
                 }
                 else if(nums2[nums2Counter] == nums1[nums1Counter]){
-                    newArray.add(nums2[nums2Counter]);
-                    newArray.add(nums2[nums2Counter]);
+                    combinedOrderedList.add(nums2[nums2Counter]);
+                    combinedOrderedList.add(nums2[nums2Counter]);
                     nums1Counter++;
                     nums2Counter++;
                 }
             }
+            // if counter2 has reached the end of array 2
             else if(nums1Counter < nums1.length && nums2Counter == nums2.length){
-                newArray.add(nums1[nums1Counter]);
+                // only add the nums1 value to combined orderedlist
+                combinedOrderedList.add(nums1[nums1Counter]);
                 nums1Counter++;
             }
+            // if counter1 has reached the end of array 1
              else if(nums2Counter < nums2.length && nums1Counter == nums1.length){
-                newArray.add(nums2[nums2Counter]);
+                //only add the nums2 value to combined orderedlist
+                combinedOrderedList.add(nums2[nums2Counter]);
                 nums2Counter++;
             }
             
@@ -42,19 +51,20 @@ class Solution {
         }
         
         // after numbers are put into one arrayList, get the size         
-        int arraySize=newArray.size();
+        int arraySize=combinedOrderedList.size();
         double medianResult = 0;
         //if size of list is even
         if(arraySize%2==0){
             // result is the parsed double value of the middle index and middle index -1 all divided by 2
-            medianResult=Double.valueOf(newArray.get(arraySize/2)+newArray.get(arraySize/2-1))/2;
+            medianResult=Double.valueOf(combinedOrderedList.get(arraySize/2)+combinedOrderedList.get(arraySize/2-1))/2;
         }
         //if size of list is odd
         else {
             //  result is the middle value in list
-            medianResult = newArray.get(arraySize/2);
+            medianResult = combinedOrderedList.get(arraySize/2);
         }
         
+        //return value
         return medianResult;
     }
 }
